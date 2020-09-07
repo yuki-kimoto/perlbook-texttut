21d1a2f7adfd293d06549d541ac5753ae36664d8
Yuki Kimoto <myapp@tk2-250-34842.vs.sakura.ne.jp>
2020-09-08 06:49:47 +0900

    PDFをA5の設定にして書き始めた

--- serve.pl
+++ serve.pl
@@ -11,5 +11,8 @@ Giblog->build;
 # Mojolicious::Lite Application
 my $app = app;
 
+system('/usr/local/bin/wkhtmltopdf --lowquality --page-size A5 --margin-bottom 5mm --margin-left 10mm --margin-right 10mm --margin-top 5mm public/index.html public/output.pdf') == 0
+  or die;
+
 # Serve
 Giblog->serve($app);
