7418cf85141ab1120f0142f176e3c8901005347c
Yuki Kimoto <kimoto.yuki@gmail.com>
2020-09-14 17:05:52 +0900

    ページ番号が入るようになった

--- concat_pdf.pl
+++ concat_pdf.pl
@@ -30,6 +30,27 @@ if ($all_pdf->pages % 2 != 0) {
   $all_pdf->page;
 }
 
+# ページ番号を挿入(はじめにから)
+for (my $page_number = 3; $page_number <= $all_pdf->pages; $page_number++) {
+  my $font = $all_pdf->corefont('Helvetica');
+  my $page = $all_pdf->openpage($page_number);
+  my $text_page_number = $page->text();
+  $text_page_number->font($font, 10);
+  
+  # 表示ページ番号
+  my $display_page_number = $page_number - 2;
+
+  # 右開き奇数
+  if ($display_page_number % 2 != 0) {
+    $text_page_number->translate(385, 20);
+  }
+  # 左開き偶数
+  else {
+    $text_page_number->translate(30, 20);
+  }
+  $text_page_number->text($display_page_number);
+}
+
 # 全体を出力
 my $all_pdf_file = "$FindBin::Bin/public/book.pdf";
 $all_pdf->saveas($all_pdf_file);
