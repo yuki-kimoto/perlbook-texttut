f1f829f31b20dc23783b27d8be713e60f7c19a86
Yuki Kimoto <kimoto.yuki@gmail.com>
2020-09-14 16:40:45 +0900

    章ごとに偶数で構成できるようになった

--- concat_pdf.pl
+++ concat_pdf.pl
@@ -5,11 +5,31 @@ use FindBin;
 use PDF::API2;
 
 my $all_pdf = PDF::API2->new();
+
+# A5サイズ
+$all_pdf->mediabox('A5');
+
+# 見開き(ページを偶数にする)
 my $top_pdf = PDF::API2->open("$FindBin::Bin/public/top.pdf");
 $all_pdf->import_page($top_pdf);
+if ($all_pdf->pages % 2 != 0) {
+  $all_pdf->page;
+}
 
+# はじめに(ページを偶数にする)
 my $beginning_pdf = PDF::API2->open("$FindBin::Bin/public/beginning.pdf");
 $all_pdf->import_page($beginning_pdf);
+if ($all_pdf->pages % 2 != 0) {
+  $all_pdf->page;
+}
 
+# 第１章(ページを偶数にする)
+my $chapter1_pdf = PDF::API2->open("$FindBin::Bin/public/chapter1.pdf");
+$all_pdf->import_page($chapter1_pdf);
+if ($all_pdf->pages % 2 != 0) {
+  $all_pdf->page;
+}
+
+# 全体を出力
 my $all_pdf_file = "$FindBin::Bin/public/book.pdf";
 $all_pdf->saveas($all_pdf_file);
--- serve.pl
+++ serve.pl
@@ -23,8 +23,5 @@ system('/usr/local/bin/wkhtmltopdf --lowquality --page-size A5 --margin-bottom 1
 system('/usr/local/bin/wkhtmltopdf --lowquality --page-size A5 --margin-bottom 10mm --margin-left 10mm --margin-right 10mm --margin-top 10mm public/chapter1.html public/chapter1.pdf') == 0
   or die;
 
-# system('/usr/local/bin/wkhtmltopdf --lowquality --page-size A5 --margin-bottom 0mm --margin-left 0mm --margin-right 0mm --margin-top 0mm public/index.html public/output.pdf') == 0
-#  or die;
-
 # Serve
 Giblog->serve($app);
