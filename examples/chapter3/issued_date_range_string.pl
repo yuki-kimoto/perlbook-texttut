b5135d3548d300061e0ea54df21a38a710d51136
Yuki Kimoto <kimoto.yuki@gmail.com>
2021-02-09 08:08:53 +0900

    文字列比較で日付を指定する方法を追加

---
+++ examples/chapter3/issued_date_range_string.pl
@@ -0,0 +1,36 @@
+# contain_perl.pl
+use strict;
+use warnings;
+use utf8;
+use Encode 'encode', 'decode';
+
+# DATAセクションから行を一行ずつ読み込む
+while (my $line = <>) {
+
+  # 一行目は読み飛ばす($.はファイルの行番号。nextで次のループの先頭へ)
+  if ($. == 1) { next }
+
+  # PerlのUTF-8を内部文字列へデコード
+  $line = decode('UTF-8', $line);
+  
+  # 改行を削除
+  chomp $line;
+
+  # カンマで分割して各変数へ
+  my ($id, $name, $author, $price, $issued_date) = split(/,/, $line);
+  
+  # 正規表現で発売日の年の部分を取得
+  my $issued_year;
+  if ($issued_date =~ /^(\d{4})/) {
+    $issued_year = $1;
+  }
+  
+  # 2018年1月1日から2019年12月31の間に発売された
+  if ($issued_date ge '2018-01-01' && $issued_date le '2019-12-31') {
+    # 出力行を作成
+    my $output_line = join(',', $id, $name, $author, $price, $issued_date);
+
+    # Perlの内部文字列をUTF-8にエンコードして出力
+    print encode('UTF-8', $output_line) . "\n";
+  }
+}
