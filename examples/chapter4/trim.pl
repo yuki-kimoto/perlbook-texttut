07683af1528c5f4f34074d92ee1c20e9fa561a70
Yuki Kimoto <kimoto.yuki@gmail.com>
2021-02-18 08:08:27 +0900

    前後の空白文字を取り除くを追加

---
+++ examples/chapter4/trim.pl
@@ -0,0 +1,14 @@
+use strict;
+use warnings;
+use utf8;
+use Encode 'encode';
+
+my $text = '　   Linuxサーバー管理入門　  ';
+
+# 先頭から複数の空白を取り除く
+$text =~ s/^\s+//;
+
+# 末尾から複数の空白を取り除く
+$text =~ s/\s+$//;
+
+print encode('UTF-8', "$text\n");
