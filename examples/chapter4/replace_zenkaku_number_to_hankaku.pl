ed00bdd5ea73302ae78217d0178a211ede4cbc06
Yuki Kimoto <kimoto.yuki@gmail.com>
2021-02-26 08:07:57 +0900

    全角数字を半角数字に変換するを追加

---
+++ examples/chapter4/replace_zenkaku_number_to_hankaku.pl
@@ -0,0 +1,20 @@
+use strict;
+use warnings;
+use utf8;
+use Encode 'encode', 'decode';
+
+# ファイル全部を読み込む
+my $in_html;
+{
+  local $/ = undef;
+  $in_html = <>;
+  $in_html = decode('UTF-8', $in_html);
+}
+
+# ファイルの内容を出力用のHTMLにコピー
+my $out_html = $in_html;
+
+# 全角数字を半角数字に置換
+$out_html =~ tr/０-９/0-9/;
+
+print encode('UTF-8', "$out_html\n");
