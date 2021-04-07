a4c614913ded1ba2800589d34ce35fce18d9fc6c
Yuki Kimoto <kimoto.yuki@gmail.com>
2021-04-08 08:14:02 +0900

    if文を途中まで校正完了

---
+++ examples/chapter2/if_name_eq_perl.pl
@@ -0,0 +1,9 @@
+use strict;
+use warnings;
+
+# 本の名前が「Perl」だったら「Name is Perl」と画面に出力
+my $book_name = 'Perl';
+
+if ($book_name eq 'Perl') {
+  print "Name is Perl\n";
+}
---
+++ examples/chapter2/if_price_under_1000.pl
@@ -0,0 +1,9 @@
+use strict;
+use warnings;
+
+# 本の価格が1000円以下だったら「Low Price」と画面に出力
+my $book_price = 900;
+
+if ($book_price <= 900) {
+  print "Low Price\n";
+}
