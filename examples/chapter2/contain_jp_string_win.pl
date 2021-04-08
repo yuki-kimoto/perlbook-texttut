a7f9f6d0a649495d17403bafd4afdc1e8812379b
Yuki Kimoto <kimoto.yuki@gmail.com>
2021-04-09 08:11:16 +0900

    文字列の項目の校正を完了

---
+++ examples/chapter2/contain_jp_string_win.pl
@@ -0,0 +1,12 @@
+use strict;
+use warnings;
+use utf8;
+use Encode 'encode';
+
+# 著者の名前に木本を含んでいたら「これは木本の著作です。」と画面に出力
+my $author_name = '木本裕紀';
+
+if ($author_name =~ /木本/) {
+  my $message = "これは木本の著作です";
+  print encode('cp932', $message) . "\n";
+}
